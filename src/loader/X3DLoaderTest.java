51aa4e003c01db2e9b76be5e96eda6dbf0bdc032
marianobesio <marianobesio@8aed0b78-db94-11dd-97dc-47b2c80cffee>
2009-01-14 19:35:24 +0000

    cambios en los archivos de x3d, porque se perchaba y usaba cosas de internet y demás problemas

--- src/loader/X3DLoaderTest.java
+++ src/loader/X3DLoaderTest.java
@@ -2,8 +2,6 @@ package loader;
 
 import com.jme.app.SimpleGame;
 
-import mainloop.Pinball;
-
 public class X3DLoaderTest extends SimpleGame//Pinball
 {
     public static void main( String[] args )
@@ -16,7 +14,10 @@ public class X3DLoaderTest extends SimpleGame//Pinball
     @Override
     protected void simpleInitGame()
     {
-        rootNode.attachChild( X3DLoader.loadScene( "./pinball.x3d", System.getProperty( "user.dir" ) ) );
+        String texDir = System.getProperty( "user.dir" ) + "/resources/textures";
+System.out.println(texDir);        
+        rootNode.attachChild( X3DLoader.loadScene( "./resources/models/Room.x3d", texDir ) );
+        rootNode.attachChild( X3DLoader.loadScene( "./resources/models/Machine.x3d", texDir ) );
         //super.simpleInitGame();
         
     }
