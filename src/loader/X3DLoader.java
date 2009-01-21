dd5ddd4c7e375e5e63af8f292c7239108a109b14
marianobesio <marianobesio@8aed0b78-db94-11dd-97dc-47b2c80cffee>
2009-01-21 17:20:29 +0000

    commit ftp

--- src/loader/X3DLoader.java
+++ src/loader/X3DLoader.java
@@ -1,51 +1,64 @@
 package loader;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.logging.Level;
+import java.util.Map;
 import java.util.logging.Logger;
 
-import com.jme.light.LightNode;
-import com.jme.light.SimpleLightNode;
 import com.jme.scene.Node;
-import com.jme.scene.Spatial;
 import com.jme.scene.state.LightState;
-import com.jme.util.export.binary.BinaryImporter;
 import com.jmex.model.converters.DTDResolver;
 import com.jmex.physics.PhysicsSpace;
 
 /**
  * Una clase que wrappea el loader de jme para trabajar todos los x3d igual
+ * 
  * @author Mariano
  */
 public class X3DLoader
 {
-    private static final Logger logger = Logger.getAnonymousLogger();
+    // TODO poner loggers posta
+    private static final Logger      logger        = Logger.getAnonymousLogger();
+
+    private InputStream              x3d;
+
+    private URL                      textureDirURL = null;
+
+    private Map<String, InputStream> dtds          = null;
+
+    private PhysicsSpace             physicsSpace  = null;
+
+    private LightState               lightState    = null;
+
+    private String                   x3dFileName   = null;
+
+    public X3DLoader( String x3dFilename ) throws FileNotFoundException
+    {
+        try
+        {
+            this.x3d = new FileInputStream( x3dFilename );
+            this.x3dFileName = x3dFilename;
+        }
+        catch ( FileNotFoundException e )
+        {
+            logger.severe( "file: " + x3dFilename + " does not exist" );
+            throw e;
+        }
+    }
 
     /**
-     * Esta funcion carga el archivo especificado buscando texturas en el
-     * directorio textureDir
-     * @param x3dFilename
-     *          archivo a cargar
-     * @param textureDir
-     *          directorio de texturas
-     * @return
-     *          la escena cargada en formato jme 
+     * Esta funcion carga el archivo especificado buscando texturas en el directorio textureDir
+     * 
+     * @return la escena cargada en formato jme
      */
-    // TODO sacar el static y hacer los parametros por setter
-    public static Node loadScene( String x3dFilename, String textureDir, LightState lightState, PhysicsSpace physicsSpace )
+    public Node loadScene()
     {
         //logger.info( "Entering loadScene" );
-        
+
         // creamos el nodo de la escena
         Node node = null;
 
@@ -60,100 +73,163 @@ public class X3DLoader
             logger.severe( "Can't instantiate X3D converter" );
             return new Node();
         }
-        
+
         // tratamos de usar dtds locales para evitar la conexion a internet
-        DTDResolver dtds;
-        try
+        if ( this.dtds != null )
         {
-            dtds = X3DLoader.getDTDResolver();
-            converter.setDTDResolver( dtds );
+            DTDResolver resolver = new DTDResolver( dtds );
+            converter.setDTDResolver( resolver );
         }
-        catch ( FileNotFoundException e2 )
+        else
         {
-            logger.info( e2.getMessage() );
-            logger.info( "Could not find local dtds, switching to remote..." );
-        }
-        
-        
-        // armamos la URL del directorio de las texturas
-        URL textureDirURL;
-        try
-        {
-            textureDirURL = new URL( "file://" + textureDir + '/' );
-        }
-        catch ( MalformedURLException e1 )
-        {
-            logger.severe( "textureDir: " + textureDir + " is invalid" );
-            return new Node();
-        }
-        
-        // seteamos la url en el conversor
-        converter.setProperty( "textures", textureDirURL );        
-        converter.setPhysicSpace( physicsSpace );
-        InputStream IP;
-        try
-        {
-            IP = new FileInputStream( x3dFilename );
-        }
-        catch ( FileNotFoundException e )
-        {
-            logger.severe( "file: " + x3dFilename + " does not exist" );
-            return new Node();
+            try
+            {
+                converter.setDTDResolver( X3DLoader.getStandardDTDResolver() );
+            }
+            catch ( FileNotFoundException e )
+            {
+                logger.info( "Could not find local dtds, switching to remote..." );
+            }
         }
 
-//        ByteArrayOutputStream BO = new ByteArrayOutputStream();
+        // seteamos las properties en el conversor
+        if ( this.textureDirURL != null )
+        {
+            converter.setProperty( "textures", textureDirURL );
+        }
+        else
+        {
+            converter.setProperty( "textures", X3DLoader.getStandardTextureDir() );
+        }
+        converter.setProperty( "physicsSpace", physicsSpace );
 
-        
         //logger.info( "Starting to convert .x3d to .jme" );
         try
         {
-            node = (Node)converter.loadScene( IP, null, lightState );
+            node = (Node) converter.loadScene( this.x3d, null, this.lightState );
         }
         catch ( Exception e )
         {
-            logger.severe( "Converting " + x3dFilename + ": could not parse file." );
             e.printStackTrace();
-            System.exit( 1 );
+            logger.severe( "Converting " + this.x3dFileName + ": could not parse file." );
             return new Node();
         }
         //logger.info( "Done converting" );
 
-        //logger.info( "Start Loading" );
-//        try
-//        {
-//            node = (Node) BinaryImporter.getInstance().load( new ByteArrayInputStream( BO.toByteArray() ) );
-//        }
-//        catch ( IOException e )
-//        {
-//            logger.logp( Level.SEVERE, X3DLoader.class.toString(), "loadScene( String x3dFilename, String textureDir )", "IOException", e );
-//            return new Node();
-//        }
-        //logger.info( "Finished loading" ) );
-        
         return node;
     }
-    
+
+    public PhysicsSpace getPhysicsSpace()
+    {
+        return physicsSpace;
+    }
+
+    public void setPhysicsSpace( PhysicsSpace physicsSpace )
+    {
+        this.physicsSpace = physicsSpace;
+    }
+
+    public LightState getLightState()
+    {
+        return lightState;
+    }
+
+    public void setLightState( LightState lightState )
+    {
+        this.lightState = lightState;
+    }
+
     /**
-     * Armamos un hash de archivos que esperamos que estén localmente para
-     * resolver los dtds más rápido.
+     * Setter del mapa de dtds e inputstreams
+     * 
+     * @param dtds
+     */
+    public void setDTDMap( Map<String, InputStream> dtds )
+    {
+        this.dtds = dtds;
+    }
+
+    /**
+     * Getter del mapa de dtds
+     * 
+     * @return
+     */
+    public Map<String, InputStream> getDTDMap()
+    {
+        return this.dtds;
+    }
+
+    /**
+     * Con esta funcion se evita tener que agregar entradas al mapa desde afuera Se pueden poner dtds y xsd
+     * 
+     * @param key
+     *            el nombre del dtd/xsd
+     * @param path
+     *            la ruta al archivo en el sistema
+     * @throws FileNotFoundException
+     *             si no encuentra el archivo lanza file not found
+     */
+    public void putDTD( String key, String path ) throws FileNotFoundException
+    {
+        if ( this.dtds == null )
+        {
+            this.dtds = new HashMap<String, InputStream>();
+        }
+        this.dtds.put( key, new FileInputStream( path ) );
+    }
+
+    /**
+     * Setter del directorio de texturas
+     * 
+     * @param path
+     */
+    public void setTextureDir( String path ) throws MalformedURLException
+    {
+        this.textureDirURL = new URL( "file://" + path + '/' );
+    }
+
+    /**
+     * Armamos un hash de archivos que esperamos que estén localmente para resolver los dtds más rápido.
+     * 
      * @return
      * @throws FileNotFoundException
      */
-    private static DTDResolver getDTDResolver() throws FileNotFoundException
+    public static DTDResolver getStandardDTDResolver() throws FileNotFoundException
     {
         DTDResolver ret;
 
         HashMap<String, InputStream> dtds = new HashMap<String, InputStream>();
 
         dtds.put( "x3d-3.0.dtd", new FileInputStream( "./resources/models/dtd/x3d-3.0.dtd" ) );
-        dtds.put( "x3d-3.0-InputOutputFields.dtd", new FileInputStream( "./resources/models/dtd/x3d-3.0-InputOutputFields.dtd" ) );
-        dtds.put( "x3d-3.0-Web3dExtensionsPrivate.dtd", new FileInputStream( "./resources/models/dtd/x3d-3.0-Web3dExtensionsPrivate.dtd" ) );
-        dtds.put( "x3d-3.0-Web3dExtensionsPublic.dtd", new FileInputStream( "./resources/models/dtd/x3d-3.0-Web3dExtensionsPublic.dtd" ) );
+        dtds.put( "x3d-3.0-InputOutputFields.dtd", new FileInputStream(
+            "./resources/models/dtd/x3d-3.0-InputOutputFields.dtd" ) );
+        dtds.put( "x3d-3.0-Web3dExtensionsPrivate.dtd", new FileInputStream(
+            "./resources/models/dtd/x3d-3.0-Web3dExtensionsPrivate.dtd" ) );
+        dtds.put( "x3d-3.0-Web3dExtensionsPublic.dtd", new FileInputStream(
+            "./resources/models/dtd/x3d-3.0-Web3dExtensionsPublic.dtd" ) );
         dtds.put( "x3d-3.0.xsd", new FileInputStream( "./resources/models/dtd/x3d-3.0.xsd" ) );
-        
 
         ret = new DTDResolver( dtds );
 
         return ret;
     }
+
+    /**
+     * Retorna los el directorio default para las texturas
+     * 
+     * @return
+     */
+    public static URL getStandardTextureDir()
+    {
+        String texDir = "file://" + System.getProperty( "user.dir" ) + "/resources/textures/";
+        try
+        {
+            return new URL( texDir );
+        }
+        catch ( MalformedURLException e )
+        {
+            return null;
+        }
+    }
+
 }
