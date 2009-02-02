bc1f839453f0367de5cb2efc850623c3f1db09d0
imscena <imscena@8aed0b78-db94-11dd-97dc-47b2c80cffee>
2009-02-02 03:10:59 +0000

    quite debugs

--- trunk/src/components/Sensor.java
+++ trunk/src/components/Sensor.java
@@ -4,9 +4,6 @@ import gamestates.PinballGameState;
 import com.jme.input.action.InputAction;
 import com.jme.input.action.InputActionEvent;
 import com.jme.input.util.SyntheticButton;
-import com.jme.math.Quaternion;
-import com.jme.math.Vector3f;
-import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Geometry;
 import com.jme.scene.Node;
 import com.jmex.physics.DynamicPhysicsNode;
@@ -35,16 +32,15 @@ private static final long serialVersionUID = 1L;
 	// Tiempo de la ultima colision considerada (donde se llamo a la logica del juego) entre una bola y este door
 	private long lastConsideredCollisionTime = 0;
 	
-	// TODO ver si el tiempo elegido funciona al tener la version final de la mesa
 	// Ventana de tiempo dentro de la cual dos colisiones seran consideradas la misma. Medido en mseg
 	// El tiempo debe ser grande pq la cantidad de colisiones detectadas depende de la velocidad de la bola
 	private static final long windowTimeForCollisions = 5000; 
 	
 
 	/**
-     * Crea un nodo dinamico que actua de sensor. Con estatico no funciona!
+     * Crea un nodo dinamico que actua de sensor.
 	 * @param pinball El juego.
-	 * @param name Nombre del nodo de molinete.
+	 * @param name Nombre del nodo sensor.
 	 * @param visualModel Modelo visual del sensor (si se desea se lo hace transparente).
 	 * @return El nodo creado.
      */
@@ -81,7 +77,6 @@ private static final long serialVersionUID = 1L;
         		
         		final ContactInfo contactInfo = ( (ContactInfo) evt.getTriggerData() );
                 DynamicPhysicsNode ball;
-                //StaticPhysicsNode s;
 
                 // El contacto pudo haber sido bola -> sensor o sensor -> bola
                 if ( contactInfo.getNode2() instanceof DynamicPhysicsNode && contactInfo.getNode2().getName() != null && contactInfo.getNode2().getName().equals(PinballGameState.PHYSIC_NODE_NAME_FOR_BALLS) /*&&
@@ -89,14 +84,12 @@ private static final long serialVersionUID = 1L;
                 { 
                     // fue sensor -> bola
                     ball = (DynamicPhysicsNode) contactInfo.getNode2();  
-                    //s = (StaticPhysicsNode) contactInfo.getNode1();  
                 }
                 else if ( contactInfo.getNode1() instanceof DynamicPhysicsNode && contactInfo.getNode1().getName() != null && contactInfo.getNode1().getName().equals(PinballGameState.PHYSIC_NODE_NAME_FOR_BALLS) /*&&
                 		     contactInfo.getNode2() instanceof StaticPhysicsNode && contactInfo.getNode2().getName() != null && contactInfo.getNode2().getName().equals("SensorPerder")*/) 
                 {
                 	// fue bola -> sensor
                     ball = (DynamicPhysicsNode) contactInfo.getNode1();  
-                    //s = (StaticPhysicsNode) contactInfo.getNode2();  
                 }
                 else 
                 {
@@ -113,9 +106,6 @@ private static final long serialVersionUID = 1L;
                 // Si la diferencia con la ultima colision considerada no es menor a windowTimeForCollisions ms, la tomo como otra colision
                 if (!(lastColl != 0 && now -  lastColl < windowTimeForCollisions))
                 {   
-                    // TODO Para debug, quitar
-            		//System.out.println("2- Colision entre " + ball.getChild(0).getName() + " y " + s.getName());
-            		
             		// Dependiendo del tipo de sensor, se llama a metodos diferentes de la logica del juego
             		if (sensor.getSensorType() == SensorType.LOST_BALL_SENSOR)
             		{    		
