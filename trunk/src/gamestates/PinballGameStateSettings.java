6a465b9178abad078f204eff5d137a954e4392b0
imscena <imscena@8aed0b78-db94-11dd-97dc-47b2c80cffee>
2009-02-02 23:50:08 +0000

    cambie la posicion inicial de la camara para que se vea mejor la mesa, logica de extraball con menos bugs. Queda pendiente el de la perdida de dos pelotas casi simultaneamente

--- trunk/src/gamestates/PinballGameStateSettings.java
+++ trunk/src/gamestates/PinballGameStateSettings.java
@@ -8,7 +8,7 @@ public class PinballGameStateSettings
 {
 	private static float camMoveSpeedDefault = 15f, camTurnSpeedDefault = 0.5f, inclinationAngleDefault = 0.0f;
 	
-	private static Vector3f camStartPosDefault = new Vector3f(0.0f, 15.0f, 9.0f), camStartLookAtDefault = new Vector3f(0.0f, 8.5f, 0.0f); 
+	private static Vector3f camStartPosDefault = new Vector3f(/*0.0f, 15.0f, 9.0f*/ 0f, 18.64f, 7.41f), camStartLookAtDefault = new Vector3f(0.0f, /*8.5f*/ 10f, 0.0f); 
 	
 	private Vector3f camStartPos, camStartLookAt;
 	
