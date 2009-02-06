a1a212230cd6f10243d3d96bc6be5e9c19005dbb
imscena <imscena@8aed0b78-db94-11dd-97dc-47b2c80cffee>
2009-02-06 03:00:30 +0000

    un iman como la gente para todos los niveles de inclinacion del juego

--- src/components/Magnet.java
+++ src/components/Magnet.java
@@ -10,7 +10,6 @@ import com.jmex.physics.PhysicsSpace;
 import com.jmex.physics.PhysicsUpdateCallback;
 import com.jmex.physics.StaticPhysicsNode;
 
-
 /**
  * Componente magnet, actua como un iman atrayendo a la bola.
  */
@@ -22,14 +21,13 @@ public class Magnet extends Node implements ActivableComponent
 	private Geometry visualModel;
 	
 	// Intensidad de la fuerza. Seteada desde el codigo de abajo en funcion del angulo de inclinacion
-	private static int force;// = 1000; //TODO antes 500 Quizas sea bueno hacerla directamente proporcional al angulo de inclinacion
-	
-	// TODO hacer forceFieldRadius y maxRadius acordes a los valores y dimensiones que tengan finalmente los objetos de la mesa
+	private static int force;
+
 	// Radio del campo de la fuerza magnetica
 	private static float forceFieldRadius = 4f; // La bola tiene radio 0.25 
 	
 	// Constante de proporcionalidad para el calculo de la intensidad de la fuerza atractora
-	private static float maxRadius = 3;
+	private static float maxRadius = 3f;
 	
 	// Esta activo este magnet?
 	public boolean active = false;
@@ -41,14 +39,15 @@ public class Magnet extends Node implements ActivableComponent
 		magnetNode.setName("Magnet");
 		
 		final Magnet magnet = new Magnet(name, visualModel);
+		
         // Creo un nodo de Magnet, con todas sus caracteristicas y lo fijo al nodo fisico
 		magnetNode.attachChild(magnet);
         
         // Genero su fisica 
-		magnetNode.generatePhysicsGeometry(true);
+		magnetNode.generatePhysicsGeometry(/*true*/);		
 		
 		// La fuerza dependera del angulo de inclinacion de la mesa
-		force = mapAngleToForce(pinball.getPinballSettings().getInclinationLevel());			
+		force = mapAngleToForce(pinball.getPinballSettings().getInclinationLevel());	
 		
 		// Efecto de iman en cada paso fisico
 		pinball.getPhysicsSpace().addToUpdateCallbacks( new PhysicsUpdateCallback() {
@@ -65,7 +64,7 @@ public class Magnet extends Node implements ActivableComponent
 	 	                    // Calcular la distancia entre la bola y el iman
 	 	                    // IMPORTANTE: la distancia se calcula entre los centros de masa de los objetos
 	 	                    float distance = ball.getChild(0).getWorldTranslation().distance(magnet.getVisualModel().getLocalTranslation());
-
+	        			 	
 	 	                    // Si la distancia es menor a cierto valor, se aplica la fuerza
 	 	                    if (distance < forceFieldRadius)
 	 	                    {
@@ -73,11 +72,14 @@ public class Magnet extends Node implements ActivableComponent
 	 		                     * de la bola y del iman. El sentido de la fuerza debe ser hacia el iman.
 	 		                     */	 
 	 		                    Vector3f direction = magnet.getVisualModel().getLocalTranslation().subtract(ball.getChild(0).getWorldTranslation()).normalize();	         
-	 		                    Vector3f appliedForce = direction.mult(force*ball.getMass()).divide(100/maxRadius*distance);
 	 		                    
+	 		                    // Es inversamente proporcional a la distancia
+	 		                    Vector3f appliedForce = direction.mult(force*ball.getMass()).divide(100/maxRadius*distance);
+
 	 		                    // Bajar la intensidad de la fuerza en z para que la bola no suba, pero si se vaya para el costado
 	 		                    appliedForce.setZ(appliedForce.getZ()/2);
-	 		                    // Aplicar la fuerza atractora. Es inversamente proporcional a la distancia
+
+	 		                    // Aplicar la fuerza atractora. 
 	 		                    ball.addForce(appliedForce);                 
 	 	                    }
 	 	            }
@@ -129,7 +131,10 @@ public class Magnet extends Node implements ActivableComponent
 		return this.active;
 	}
 	
-	// Si, asi de cabeza. Todo sale de prueba y error. Aplicar un polinomio de Taylor seria ineficiente
+	/**
+	 * En base al angulo de inclinacion de la mesa calcula la constante aplicada para el computo de la intensidad 
+	 * de la fuerza atractora
+	 */
 	private static int mapAngleToForce(float angle)
 	{
 		float epsilon = 0.001f;
@@ -137,27 +142,43 @@ public class Magnet extends Node implements ActivableComponent
 		
 		if ( angle <= 1 + epsilon)
 		{
-			force = 20; //200 es muy fuerte para 1º
+			force = 20;
 		}
 		else if ( angle <= 2 + epsilon)
 		{
-			force = /*200*/40; //200 es muy fuerte para 1º
+			force = 35;
 		}
 		else if ( angle <= 3 + epsilon)
 		{
-			force = /*200*/70; //200 es muy fuerte para 1º
-		}
-		else if (angle < 6)
+			force = 60;
+		}		
+		else if ( angle <= 4 + epsilon)
 		{
-			force = 500;
+			force = 80;
 		}
-		else if (angle < 8)
+		else if ( angle <= 5 + epsilon)
 		{
-			force = 800;
+			force = 105;
 		}
-		else // Pensado para un angulo maximo de 10
+		else if (angle <= 6 + epsilon)
 		{
-			force = 1000;
+			force = 150;
+		}
+		else if (angle <= 7 + epsilon)
+		{
+			force = 185;
+		}
+		else if (angle <= 8 + epsilon)
+		{
+			force = 210;
+		}		
+		else if (angle <= 9 + epsilon)
+		{
+			force = 275;
+		}
+		else // Pensado para un angulo maximo de 10 grados
+		{
+			force = 300;
 		}
 		
 		return force;
